800107371b5da2738984416742d6b522f259cf4e
RaulMuM <rodriguezppp@gmail.com>
2022-10-24 19:52:45 +0200

    db.py con variables entorno bien configuradas

--- config/db.py
+++ config/db.py
@@ -10,13 +10,13 @@ import os
 #print(dotenv_path)
 load_dotenv()
 
-print(os.getenv("USERNAME"))
+print(os.getenv("USERNAME_DB"))
 print(os.getenv("PASSWORD_DB"))
 print(os.getenv("HOST"))
 print(os.getenv("PORT"))
 print(os.getenv("DATABASE"))
 
-engine = create_engine(f'postgresql+psycopg2://{os.getenv("USER_DB")}:{os.getenv("PASSWORD_DB")}@{os.getenv("HOST")}:{os.getenv("PORT")}/{os.getenv("DATABASE")}')
+engine = create_engine(f'postgresql+psycopg2://{os.getenv("USERNAME_DB")}:{os.getenv("PASSWORD_DB")}@{os.getenv("HOST")}:{os.getenv("PORT")}/{os.getenv("DATABASE")}')
 
 meta = MetaData()
 
